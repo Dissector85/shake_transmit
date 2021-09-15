c227783da3087403977afc0b05e07924765e20cb
Dissector85 <80616305+Dissector85@users.noreply.github.com>
2021-09-15 14:08:10 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 radio.setGroup(1)
 basic.forever(function () {
-	
+    if (input.acceleration(Dimension.Strength) > 1500) {
+        radio.sendString("on")
+        music.playTone(262, music.beat(BeatFraction.Sixteenth))
+        basic.showIcon(IconNames.Heart)
+        basic.clearScreen()
+    }
 })
